4f6702bbd46eae7ed9b670fb9bfaddd094382f72
imisumi <imisumi-wsl@imisumi>
2024-01-06 15:09:03 +0100

    code restructure

--- src/camera/camera.c
+++ src/camera/camera.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   camera.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ichiro <ichiro@student.42.fr>              +#+  +:+       +#+        */
+/*   By: imisumi-wsl <imisumi-wsl@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 02:06:12 by ichiro            #+#    #+#             */
-/*   Updated: 2023/12/23 00:04:06 by ichiro           ###   ########.fr       */
+/*   Updated: 2024/01/06 15:07:27 by imisumi-wsl      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,30 @@
 extern int	total_frames;
 
 extern uint32_t	global_frame;
-void recalculat_ray_directions(t_data *d)
+
+t_vec2f	get_pixel_coord(uint32_t width, uint32_t height, uint32_t x, uint32_t y)
 {
-	for (uint32_t y = 0; y < d->utils.height; y++)
+	t_vec2f	coord;
+
+	coord[X] = (float)x / (float)(width);
+	coord[Y] = (float)y / (float)(height);
+	return (coord);
+}
+
+void	recalculat_ray_directions(t_data *d)
+{
+	t_vec2f	coordf;
+	uint32_t	x;
+	uint32_t	y;
+
+	y = 0;
+	while (y < d->utils.height)
 	{
-		for (uint32_t x = 0; x < d->utils.width; x++)
+		x = 0;
+		while (x < d->utils.width)
 		{
-			t_vec2f coordf = {(float)x / (float)(d->utils.width), (float)y / (float)(d->utils.height)};
-			coordf = (coordf * 2.0f) - 1.0f;
+			coordf = (get_pixel_coord(d->utils.width, d->utils.height, x, y) * 2.0f) - 1.0f;
+			
 			t_vec4f target = mat4_mul_vec4f(d->scene.camera.inv_projection, (t_vec4f){coordf[X], coordf[Y], 1.0f, 1.0f});
 			t_vec3f t = (t_vec3f){target[X], target[Y], target[Z], 0.0f} / target[W];
 			target = vec4f_normalize((t_vec4f){-t[X], t[Y], -t[Z], 0.0f});
@@ -33,101 +49,47 @@ void recalculat_ray_directions(t_data *d)
 			//! dont think I need this
 			//? - Left handed coordinate system -target.x
 			// t_vec3 rayDirection = vec3_new(target.x, target.y, target.z);
-			t_vec3f rayDirectionf = {target[X], target[Y], target[Z], 0.0f};
-			d->scene.camera.ray_dir[x + y * d->utils.width] = rayDirectionf;
+
+			target[Y] = 0.0f;
+			d->scene.camera.ray_dir[x + y * d->utils.width] = target;
+			x++;
 		}
+		y++;
 	}
 }
 
-void init_camera(t_camera *cam)
+// cam->position = (t_vec3f){6.0f, 2.0, 30.0f, 0.0f};
+// cam->dir = (t_vec3f){0.0, 0.2, -1.0, 0.0};
+// cam->vertical_fov = 60.0f;
+void	init_camera(t_camera *cam)
 {
-
-	cam->position = (t_vec3f){6.0f, 2.0, 30.0f, 0.0f};
-	cam->dir = (t_vec3f){0.0, 0.2, -1.0, 0.0};
-
-	// cam->position = (t_vec3f){0.0f, 2.0, 5.0f, 0.0f};
-	// cam->dir = (t_vec3f){0.0, 0.0, -1.0, 0.0};
-
-	// cam->ray_dir = malloc(sizeof(t_vec3) * WIDTH * HEIGHT);
-
 	cam->mouse_delta[X] = 0;
 	cam->mouse_delta[Y] = 0;
-
 	cam->projection = mat4_identity();
 	cam->inv_projection = mat4_identity();
 	cam->view = mat4_identity();
 	cam->inv_view = mat4_identity();
-
-	cam->vertical_fov = 45.0f;
 	cam->aspectRatio = (float)WIDTH / (float)HEIGHT;
 	cam->zNear = 0.1f;
 	cam->zFar = 1000.0f;
-
 	cam->prev_mouse_pos[X] = -1;
 	cam->prev_mouse_pos[Y] = -1;
 }
 
-void recalculate_view(t_data *d)
+void	recalculate_view(t_data *d)
 {
-	t_vec3 campos;
-	campos.x = d->scene.camera.position[X];
-	campos.y = d->scene.camera.position[Y];
-	campos.z = d->scene.camera.position[Z];
-	
-	t_vec3 camdir;
-	camdir.x = d->scene.camera.dir[X];
-	camdir.y = d->scene.camera.dir[Y];
-	camdir.z = d->scene.camera.dir[Z];
+	const t_vec3f	pos = d->scene.camera.position;
+	const t_vec3f	dir = d->scene.camera.dir;
 
-	d->scene.camera.view = mat4_look_at(campos, \
-							vec3_add(campos, camdir), \
-							vec3_new(0.0f, 1.0f, 0.0f));
+	d->scene.camera.view = mat4_look_at_f(pos, pos + dir, \
+							(t_vec3f){0.0f, 1.0f, 0.0f, 0.0f});
 	d->scene.camera.inv_view = mat4_inverse(d->scene.camera.view);
 }
 
-void recalculated_projection(t_data *d)
+void	recalculated_projection(t_data *d)
 {
 	d->scene.camera.projection = mat4_perspective(fov_radians(d->scene.camera.vertical_fov), \
-								(float)d->utils.width / (float)d->utils.height, \
+								d->scene.camera.aspectRatio, \
 								d->scene.camera.zNear, d->scene.camera.zFar);
 	d->scene.camera.inv_projection = mat4_inverse(d->scene.camera.projection);
 }
-
-// void anti_aliasing(t_data *data)
-// {
-// 	uint32_t	rngState;
-// 	uint32_t	x;
-// 	uint32_t	y;
-
-// 	t_vec3f cam_right_f = {
-// 		data->scene.camera.view.m[0][0],
-// 		data->scene.camera.view.m[1][0],
-// 		data->scene.camera.view.m[2][0],
-// 		0.0f
-// 	};
-// 	t_vec3f cam_up_f = {
-// 		data->scene.camera.view.m[0][1],
-// 		data->scene.camera.view.m[1][1],
-// 		data->scene.camera.view.m[2][1],
-// 		0.0f
-// 	};
-
-// 	y = 0;
-// 	while (y < data->utils.height)
-// 	{
-// 		x = 0;
-// 		while (x < data->utils.width)
-// 		{
-// 			t_vec3f aa_dir = data->scene.camera.ray_target[x + y * WIDTH];
-// 			rngState = get_rngstate(data->utils.width, data->utils.height, x, y);
-			
-// 			float jit = (randomFloat(&rngState) - 0.5f) / (float)data->utils.width;
-// 			float jit2 = (randomFloat(&rngState) - 0.5f) / (float)data->utils.width;
-// 			t_vec3f jittered_f = aa_dir + (cam_right_f * jit) + (cam_up_f * jit2);
-// 			data->scene.camera.ray_dir[x + y * WIDTH] = vec3f_normalize(jittered_f);
-// 			global_frame++;
-// 			x++;
-// 		}
-// 		y++;
-// 	}
-// }
