f8a26d217193bc2ea613f2e06e262790ba409d52
imisumi <ijmisumi@gmail.com>
2024-01-24 01:57:51 +0100

    normal map

--- src/intersection/triangle_intersect.c
+++ src/intersection/triangle_intersect.c
@@ -6,107 +6,196 @@
 /*   By: imisumi-wsl <imisumi-wsl@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 00:26:12 by ichiro            #+#    #+#             */
-/*   Updated: 2024/01/23 02:27:44 by imisumi-wsl      ###   ########.fr       */
+/*   Updated: 2024/01/24 01:47:15 by imisumi-wsl      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-// https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
-bool	single_triangle_intersection(t_rayf ray, uint32_t i, t_hitinfo *hitinfo, t_scene *scene, int index)
+
+#define E1 0
+#define E2 1
+
+
+
+typedef struct {
+    float u, v, w;
+} BarycentricCoords;
+
+// t_vec3f sampleTexture(const uint8_t *rgba, int width, int height, const BarycentricCoords* baryCoords, float *uvs)
+t_vec3f	sampleTexture(mlx_texture_t *tex, const BarycentricCoords* baryCoords, float *uvs)
 {
-	const t_vec3f tri_a = {
-		scene->vertices[scene->tri_meshes[index].v_idx[i][0] * 3],
-		scene->vertices[scene->tri_meshes[index].v_idx[i][0] * 3 + 1],
-		scene->vertices[scene->tri_meshes[index].v_idx[i][0] * 3 + 2]};
-	const t_vec3f tri_b = {
-		scene->vertices[scene->tri_meshes[index].v_idx[i][1] * 3],
-		scene->vertices[scene->tri_meshes[index].v_idx[i][1] * 3 + 1],
-		scene->vertices[scene->tri_meshes[index].v_idx[i][1] * 3 + 2]};
-	const t_vec3f tri_c= {
-		scene->vertices[scene->tri_meshes[index].v_idx[i][2] * 3],
-		scene->vertices[scene->tri_meshes[index].v_idx[i][2] * 3 + 1],
-		scene->vertices[scene->tri_meshes[index].v_idx[i][2] * 3 + 2]};
-	
-	t_vec3f e1 = tri_b - tri_a;
-	t_vec3f e2 = tri_c - tri_a;
-	
-	t_vec3f ray_cross_e2 = vec3f_cross(ray[DIR], e2);
+	int width = tex->width;
+	int height = tex->height;
+	uint8_t *rgba = tex->pixels;
 
-	float det = vec3f_dot(e1, ray_cross_e2);
+    // Calculate the texture coordinates based on barycentric coordinates
+    // float texU = baryCoords->u * uvs[0] + baryCoords->v * uvs[2] + baryCoords->w * uvs[4];
+    // float texV = baryCoords->u * uvs[1] + baryCoords->v * uvs[3] + baryCoords->w * uvs[5];
+	float texU = baryCoords->w * uvs[0] + baryCoords->u * uvs[2] + baryCoords->v * uvs[4];
+    float texV = baryCoords->w * uvs[1] + baryCoords->u * uvs[3] + baryCoords->v * uvs[5];
+	texV = 1 - texV;
+    // Convert floating-point texture coordinates to integer indices
+    int x = (int)(texU * (width - 1));
+    int y = (int)(texV * (height - 1));
 
-	if (BACK_FACE_CULLING && det < 0)
-		return (false);
-	else if (det > -EPSILON && det < EPSILON)
-		return (false); // This ray is parallel to this triangle.
+    // Ensure indices are within bounds
+    x = (x < 0) ? 0 : ((x >= width) ? width - 1 : x);
+    y = (y < 0) ? 0 : ((y >= height) ? height - 1 : y);
 
-	float inv_det = 1.0f / det;
-	t_vec3f s = ray[ORIGIN] - tri_a;
-	float u = inv_det * vec3f_dot(s, ray_cross_e2);
+    // Calculate the offset into the RGBA buffer
+    int offset = (y * width + x) * 4;
 
-	if (u < 0.0f || u > 1.0f)
-		return (false);
+    // Sample the RGBA values from the buffer
+    uint8_t red = rgba[offset];
+    uint8_t green = rgba[offset + 1];
+    uint8_t blue = rgba[offset + 2];
+    uint8_t alpha = rgba[offset + 3];
+	alpha = 255;
 
-	t_vec3f s_cross_e1 = vec3f_cross(s, e1);
-	float v = inv_det * vec3f_dot(ray[DIR], s_cross_e1);
+	t_vec3f color;
+	color[R] = red / 255.0f;
+	color[G] = green / 255.0f;
+	color[B] = blue / 255.0f;
+	color[A] = 1.0f;
+    // Combine the sampled values into a 32-bit color value
+    // return (alpha << 24) | (red << 16) | (green << 8) | blue;
+	return (color);
+}
 
-	if (v < 0.0f || u + v > 1.0f)
-		return (false);
+t_vec3f	get_tri_from_index(t_scene *scene, int index, int face, int id)
+{
+	const t_vec3f	tri = {
+		scene->vertices[scene->tri_meshes[index].v_idx[face][id] * 3],
+		scene->vertices[scene->tri_meshes[index].v_idx[face][id] * 3 + 1],
+		scene->vertices[scene->tri_meshes[index].v_idx[face][id] * 3 + 2]};
+
+	return (tri);
+}
+
+// void	apply_tri_material(t_hitinfo *hitinfo, t_scene *scene, )
+// {
+// 	int mat_index = scene->tri_meshes[index].mat_idx[face];
+// 	hitinfo->material = scene->materials[mat_index];
+// 	if (scene->materials[mat_index].color_tex)
+// 	{
+// 		BarycentricCoords baryCoords = {u, v, 1.0f - u - v};
+// 		float uvs[6];
+// 		uvs[0] = scene->tex_coords[scene->tri_meshes[index].vt_idx[face][0] * 2];
+// 		uvs[1] = scene->tex_coords[scene->tri_meshes[index].vt_idx[face][0] * 2 + 1];
+		
+// 		uvs[2] = scene->tex_coords[scene->tri_meshes[index].vt_idx[face][1] * 2];
+// 		uvs[3] = scene->tex_coords[scene->tri_meshes[index].vt_idx[face][1] * 2 + 1];
+
+// 		uvs[4] = scene->tex_coords[scene->tri_meshes[index].vt_idx[face][2] * 2];
+// 		uvs[5] = scene->tex_coords[scene->tri_meshes[index].vt_idx[face][2] * 2 + 1];
+// 		hitinfo->material.color =  sampleTexture(scene->materials[mat_index].color_tex, &baryCoords, uvs);
+// 	}
+// }
 
 	// if (u < 0 || u > 1 || v < 0 || u + v > 1)
 	// 	return (false);
 	// if ((u < 0) | (u > 1) | (v < 0) | (u + v > 1))
 	// 	return (false);
-
-	// At this stage we can compute t to find out where the intersection point is on the line.
-	float t = inv_det * vec3f_dot(e2, s_cross_e1);
+// https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+bool	single_triangle_intersection(t_rayf ray, uint32_t face_idx, t_hitinfo *hitinfo, t_scene *scene, int mesh_idx)
+{
+	const t_vec3f	tri[3] = {
+		get_tri_from_index(scene, mesh_idx, face_idx, 0),
+		get_tri_from_index(scene, mesh_idx, face_idx, 1),
+		get_tri_from_index(scene, mesh_idx, face_idx, 2)};
+	const t_vec3f edge[2] = {tri[1] - tri[0], tri[2] - tri[0]};
 	
+	t_vec3f ray_cross_e2 = vec3f_cross(ray[DIR], edge[E2]);
+
+	float det = vec3f_dot(edge[E1], ray_cross_e2);
+
+	if (BACK_FACE_CULLING && det < 0)
+		return (false);
+	else if (det > -EPSILON && det < EPSILON)
+		return (false);
+
+	float inv_det = 1.0f / det;
+	t_vec3f s = ray[ORIGIN] - tri[0];
+	float u = inv_det * vec3f_dot(s, ray_cross_e2);
+
+	if (u < 0.0f || u > 1.0f)
+		return (false);
+
+	t_vec3f s_cross_e1 = vec3f_cross(s, edge[E1]);
+	float v = inv_det * vec3f_dot(ray[DIR], s_cross_e1);
+
+	if (v < 0.0f || u + v > 1.0f)
+		return (false);
+	float t = inv_det * vec3f_dot(edge[E2], s_cross_e1);
 	if (t > EPSILON && t < hitinfo->distance)
 	{
 		hitinfo->hit = true;
 		hitinfo->position = ray[ORIGIN] + ray[DIR] * t;
-		hitinfo->normal = vec3f_normalize(vec3f_cross(e1, e2));
+		hitinfo->normal = vec3f_normalize(vec3f_cross(edge[E1], edge[E2]));
 		hitinfo->distance = t;
 		
-		int mat_index = scene->tri_meshes[index].mat_idx[i];
+		int mat_index = scene->tri_meshes[mesh_idx].mat_idx[face_idx];
 		hitinfo->material = scene->materials[mat_index];
-		return (true);
+
+		BarycentricCoords baryCoords = {u, v, 1.0f - u - v};
+
+		float uvs[6];
+
+		uvs[0] = scene->tex_coords[scene->tri_meshes[mesh_idx].vt_idx[face_idx][0] * 2];
+		uvs[1] = scene->tex_coords[scene->tri_meshes[mesh_idx].vt_idx[face_idx][0] * 2 + 1];
+		
+		uvs[2] = scene->tex_coords[scene->tri_meshes[mesh_idx].vt_idx[face_idx][1] * 2];
+		uvs[3] = scene->tex_coords[scene->tri_meshes[mesh_idx].vt_idx[face_idx][1] * 2 + 1];
+
+		uvs[4] = scene->tex_coords[scene->tri_meshes[mesh_idx].vt_idx[face_idx][2] * 2];
+		uvs[5] = scene->tex_coords[scene->tri_meshes[mesh_idx].vt_idx[face_idx][2] * 2 + 1];
+
+		t_vec3f normal_color;
+		if (scene->materials[mat_index].normal_tex)
+		{
+			normal_color = sampleTexture(scene->materials[mat_index].normal_tex, &baryCoords, uvs);
+			t_vec3f normal_map_offset;
+			normal_map_offset[X] = 2.0f * normal_color[R] - 1.0f;
+			normal_map_offset[Y] = 2.0f * normal_color[G] - 1.0f;
+			normal_map_offset[Z] = 2.0f * normal_color[B] - 1.0f;
+
+			hitinfo->normal = vec3f_normalize(hitinfo->normal + normal_map_offset);
+		}
+
+		if (scene->materials[mat_index].color_tex)
+			hitinfo->material.color =  sampleTexture(scene->materials[mat_index].color_tex, &baryCoords, uvs);
+
 	}
-	return (false);
+	return (hitinfo->hit);
 }
 
-bool triangle_intersection(t_rayf ray, t_scene *scene, uint32_t index, t_hitinfo *obj_hit)
+bool triangle_intersection(t_rayf ray, t_scene *scene, uint32_t mesh_idx, t_hitinfo *hitinfo)
 {
 	int	i;
 
 	i = 0;
-	while (i < scene->tri_meshes[index].num_faces)
+	while (i < scene->tri_meshes[mesh_idx].num_faces)
 	{
-		// if (i == 2)
-		// 	break ;
-		single_triangle_intersection(ray, i, obj_hit, scene, index);
-		// t_tri tri = scene->tri_meshes[index].tris[i];
-		// printf("%f %f %f\n", tri.vertices[0], tri.vertices[1], tri.vertices[2]);
+		single_triangle_intersection(ray, i, hitinfo, scene, mesh_idx);
 		i++;
 	}
-	// exit(0);
-	return (true);
+	return (hitinfo->hit);
 }
 
 //TODO check this
-// bool	tri_mesh_intersection(t_rayf ray, t_scene *scene, t_hitinfo *obj_hit)
-// {
-// 	int	i;
+bool	tri_mesh_intersection(t_rayf ray, t_scene *scene, t_hitinfo *hitinfo)
+{
+	int	i;
 
-// 	i = 0;
-// 	while (i < vec_length(&scene->tri_meshes))
-// 	{
-// 		triangle_intersection(ray, scene, i, obj_hit);
-// 		// break ;
-// 		i++;
-// 	}
-// 	return (true);
-// }
+	i = 0;
+	while (i < vec_length(&scene->tri_meshes))
+	{
+		triangle_intersection(ray, scene, i, hitinfo);
+		i++;
+	}
+	return (hitinfo->hit);
+}
 
 
 t_hitinfo	triangle_bvh_intersection(t_rayf ray, t_hitinfo hitinfo, t_bvh_node *node, t_scene *scene, int index)
--- src/parser/obj.c
+++ src/parser/obj.c
@@ -6,7 +6,7 @@
 /*   By: imisumi-wsl <imisumi-wsl@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 01:16:18 by ichiro            #+#    #+#             */
-/*   Updated: 2024/01/23 01:42:26 by imisumi-wsl      ###   ########.fr       */
+/*   Updated: 2024/01/24 01:56:16 by imisumi-wsl      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ bool	parse_obj(t_scene *scene, const char *filename)
 
 	int i = mesh->position_count;
 	printf("Mesh has %d vertices\n", i);
+	i = mesh->texcoord_count;
+	printf("Mesh has %d texcoords\n", i);
 	i = mesh->normal_count;
 	printf("Mesh has %d normals\n", i);
 	i = mesh->face_count;
@@ -57,10 +59,27 @@ bool	parse_obj(t_scene *scene, const char *filename)
 		scene->materials[i].color[R] = mat.Kd[0];
 		scene->materials[i].color[G] = mat.Kd[1];
 		scene->materials[i].color[B] = mat.Kd[2];
-		// scene->materials[i].color[R] = 1.0f;
-		// scene->materials[i].color[G] = 1.0f;
-		// scene->materials[i].color[B] = 0.2f;
-		// scene->materials[i].color[A] = 1.0f;
+
+		// if (scene->materials[i].color_tex == NULL)
+		// 	exit(1);
+
+		char *mat_name = mesh->materials[i].map_Kd.name;
+		char *mat_path = mesh->materials[i].map_Kd.path;
+		printf("Material name: %s, %s\n", mat_name, mat_path);
+		if (mat_path)
+			scene->materials[i].color_tex = mlx_load_png(mat_path);
+		mlx_texture_t *tex = scene->materials[i].color_tex;
+		if (tex)
+			printf("Texture size: %d %d\n", tex->width, tex->height);
+		
+		char *mat_normal = mesh->materials[i].map_bump.name;
+		char *mat_normal_path = mesh->materials[i].map_bump.path;
+		printf("Material normal: %s, %s\n", mat_normal, mat_normal_path);
+		
+		if (mat_normal_path)
+			scene->materials[i].normal_tex = mlx_load_png(mat_normal_path);
+		// char *normal = "assets/obj/tex/normal.png";
+		// scene->materials[i].normal_tex = mlx_load_png(normal);
 	}
 	t_vec3f m = scene->materials[0].color;
 	printf("Material color: %f %f %f\n", m[0], m[1], m[2]);
@@ -70,6 +89,7 @@ bool	parse_obj(t_scene *scene, const char *filename)
 	// {
 		
 	// }
+
 
 	int pos = mesh->position_count;
 	for (int i = 0; i < pos; i++)
@@ -91,8 +111,8 @@ bool	parse_obj(t_scene *scene, const char *filename)
 	scene->num_tri_meshes = count;
 
 	t_tri_mesh tri_mesh;
-	t_tri	tri;
 	scene->vertices = copy_float_array(mesh->positions, mesh->position_count * 3);
+	scene->tex_coords = copy_float_array(mesh->texcoords, mesh->texcoord_count * 2);
 
 	int face_offset = 0;
 	int	mat_offset = 0;
@@ -100,35 +120,42 @@ bool	parse_obj(t_scene *scene, const char *filename)
 	for (int current = 0; current < num_shapes; current++)
 	{
 		faces = mesh->objects[current].face_count;
-		t_tri	*tris = malloc(sizeof(t_tri) * faces);
 		tri_mesh.mat_idx = malloc(sizeof(uint32_t) * faces);
 		tri_mesh.v_idx = malloc(sizeof(t_vec3ui) * faces);
+		tri_mesh.vt_idx = malloc(sizeof(t_vec3ui) * faces);
 		for (int i = 0; i < faces; i++)
 		{
 			// mesh->face_vertices should be 3
 			for (int j = 0; j < 3; j++)
 			{
 				int index = mesh->indices[face_offset + i * 3 + j].p;
-				// tri.v[j][0] = mesh->positions[index * 3 + 0];
-				// tri.v[j][1] = mesh->positions[index * 3 + 1];
-				// tri.v[j][2] = mesh->positions[index * 3 + 2];
 				
-				tri.v_idx[j] = index;
 				tri_mesh.v_idx[i][j] = index;
+				// printf("%d/", index);
+				index = mesh->indices[face_offset + i * 3 + j].t;
+				tri_mesh.vt_idx[i][j] = index;
+				// printf("%d\t", index);
 			}
+			// printf("\n");
 			if (mesh->material_count > 0)
 			{
 				int mat_idx = mesh->face_materials[mat_offset + i];
 				tri_mesh.mat_idx[i] = mat_idx;
 				// printf("mat_idx = %d\n", mat_idx);
 			}
-			tris[i] = tri;
 		}
 		tri_mesh.num_faces = faces;
-		// tri_mesh.tris = tris;
 		vec_push(&scene->tri_meshes, &tri_mesh);
 		face_offset += mesh->objects[current].face_count * 3;
 		mat_offset += mesh->objects[current].face_count;
 	}
+
+
+	// for (int i = 2; i < mesh->texcoord_count * 2; i+=2)
+	// {
+	// 	printf("Texcoord %d: %f %f\n", i, scene->tex_coords[i], scene->tex_coords[i + 1]);
+	// 	if (i % 3 == 2)
+	// 		printf("\n");
+	// }
 	return (true);
 }
