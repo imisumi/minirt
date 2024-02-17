9e3cc021b491bae824f408cab0a1f4c46720fa70
imisumi <ichiromisumi@outlook.com>
2024-02-17 17:24:05 +0100

    norm

--- src/parser/camera.c
+++ src/parser/camera.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   camera.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: imisumi <imisumi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: imisumi-wsl <imisumi-wsl@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 00:29:56 by ichiro            #+#    #+#             */
-/*   Updated: 2024/01/09 13:54:56 by imisumi          ###   ########.fr       */
+/*   Updated: 2024/02/16 22:17:56 by imisumi-wsl      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,17 @@
 //?		  0.0,0.0,1.0
 //?		∗ FOV : Horizontal field of view in degrees in range [0,180]: 70
 
-
+//TODO 0 - 180 weird redshift does 0.2 - 174
 bool	parse_camera(char **split, t_camera *camera)
 {
-	// printf("Camera parser\n");
 	if (ft_split_count(split) != 4)
-	{
-		print_error("camera arg count");
-		return (false);
-	}
+		return (print_error("camera arg count"));
 	if (parse_vec3(split[1], &camera->position) == false)
-	{
-		print_error("camera position");
-		return (false);
-	}
+		return (print_error("camera position"));
 	if (parse_vec3_normal(split[2], &camera->dir) == false)
-	{
-		print_error("camera direction out of range [-1, 1]");
-		return (false);
-	}
+		return (print_error("camera direction out of range [-1, 1]"));
 	if (ft_stof(split[3], &camera->vertical_fov) == false)
 		return (print_error("camera fov"), false);
-	//TODO 0 - 180 weird redshift does 0.2 - 174
 	if (camera->vertical_fov < 0.0f || camera->vertical_fov > 180.0f)
 		return (print_error("camera fov range"), false);
 	return (true);
--- src/parser/light.c
+++ src/parser/light.c
@@ -6,7 +6,7 @@
 /*   By: imisumi-wsl <imisumi-wsl@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 00:29:56 by ichiro            #+#    #+#             */
-/*   Updated: 2024/01/20 14:56:46 by imisumi-wsl      ###   ########.fr       */
+/*   Updated: 2024/02/17 16:28:11 by imisumi-wsl      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,14 @@
 
 bool	parse_ambient_light(char **split, t_scene *scene)
 {
-	// printf("Ambient light parser\n");
 	if (ft_split_count(split) != 3)
-	{
-		print_error("ambient light arg count");
-		return (false);
-	}
+		return (print_error("ambient light arg count"));
 	if (ft_stof(split[1], &scene->ambient_light) == false)
-	{
-		print_error("ambient light intensity");
-		return (false);
-	}
+		return (print_error("ambient light intensity"));
 	if (scene->ambient_light < 0.0f || scene->ambient_light > 1.0f)
-	{
-		print_error("ambient light intensity range");
-		return (false);
-	}
+		return (print_error("ambient light intensity range"));
 	if (parse_8bit_color(split[2], &scene->ambient_color) == false)
-	{
-		print_error("ambient light color");
-		return (false);
-	}
+		return (print_error("ambient light color"));
 	return (true);
 }
 
@@ -54,45 +41,24 @@ bool	parse_light(char **split, t_scene *scene)
 {
 	t_point_light	point_light;
 
-	// printf("Light parser\n");
 	if (ft_split_count(split) != 4)
-	{
-		print_error("light arg count");
-		return (false);
-	}
+		return (print_error("light arg count"));
 	if (parse_vec3(split[1], &point_light.position) == false)
-	{
-		print_error("light position");
-		return (false);
-	}
+		return (print_error("light position"));
 	if (ft_stof(split[2], &point_light.intensity) == false)
-	{
-		print_error("light brightness");
-		return (false);
-	}
+		return (print_error("light intensity"));
 	if (parse_8bit_color(split[3], &point_light.color) == false)
-	{
-		print_error("light color");
-		return (false);
-	}
+		return (print_error("light color"));
 	vec_push(&scene->point_lights, &point_light);
 	return (true);
 }
-
-	// printf("--------------\n");
-	// printf("%f, %f, %f\n", point_light.position[X], \
-//		point_light.position[Y], point_light.position[Z]);
-	// printf("%f\n", point_light.intensity);
-	// printf("%f, %f, %f\n", point_light.color[R], point_light.color[G], \
-//		point_light.color[B]);
-
 
 bool	parse_load_hdri(const char *path, t_hdri *hdri)
 {
 	int			ret;
 	int			width;
 	int			height;
-	const char*	err;
+	const char	*err;
 
 	ret = LoadEXR(&hdri->rgba, &hdri->width, &hdri->height, path, NULL);
 	if (ret != 0)
@@ -105,29 +71,22 @@ bool	parse_load_hdri(const char *path, t_hdri *hdri)
 	return (true);
 }
 
-
 bool	parse_hdri(char **split, t_scene *scene)
 {
 	t_hdri		hdri;
 	static bool	once = false;
 
-	// printf("HDRI parser\n");
 	if (once == true)
 	{
-		printf("Warning: hdri already defined, only first definition will be used\n");
+		print_warning("hdri already defined,\
+			only first definition will be used");
 		return (true);
 	}
 	if (ft_split_count(split) != 2)
-	{
-		print_error("hdri arg count");
-		return (false);
-	}
+		return (print_error("hdri arg count"));
 	printf("split[1]: %s\n", split[1]);
 	if (parse_load_hdri(split[1], &hdri) == false)
-	{
-		print_error("hdri path");
-		return (false);
-	}
+		return (print_error("hdri path"));
 	once = true;
 	scene->hdri = hdri;
 	return (true);
